809f80d0e9f1ea96b044c7c01e3690bef8398e9b
Jeremy <jeremyretamararrieta@gmail.com>
2019-11-04 15:10:24 -0800

    setting up skeleton

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
+import Header from './Header.js';
 import './App.css';
 
 
@@ -23,10 +24,11 @@ function Copyright() {
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
-          We provide Software contract services, cloud buisness infrastructure and data analytics solutions.  
+          Contact us today for a free quote:  
         </Typography>
         <Copyright />
       </Box>
